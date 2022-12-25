081358c34a5d5b6dff9857c4ce21859b468542d0
Well-Script <91045557+WellOfficial@users.noreply.github.com>
2022-12-25 20:13:10 +0700

    Update Library.lua

--- main/Library.lua
+++ main/Library.lua
@@ -31,7 +31,7 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(0, 15, 30);
     BackgroundColor = Color3.fromRGB(5, 5, 20);
-    AccentColor = Color3.fromRGB(255, 51, 51);
+    AccentColor = Color3.fromRGB(0, 180, 240);
     OutlineColor = Color3.fromRGB(0, 0, 5);
 
     OpenedFrames = {};
